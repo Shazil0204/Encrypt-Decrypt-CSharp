3d73ab095470dfa294f431690cb567fb3be040c7
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-03-14 10:26:20 +0100

    Complete

---
+++ Decrypt/Decrypt/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class DecryptionProgram
+{
+    // Decrypt data using AES algorithm
+    public static string Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+    {
+        // Create AES decryption algorithm instance
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            // Create decryptor using AES algorithm
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create memory stream to store decrypted data
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                // Create crypto stream to perform decryption
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    // Read decrypted data from crypto stream
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the encrypted text you received:");
+        string encryptedText = Console.ReadLine();
+
+        // Define decryption key and initialization vector (IV)
+        byte[] encryptedData = Convert.FromBase64String(encryptedText);
+        byte[] key = Encoding.UTF8.GetBytes("0123456789ABCDEF"); // Same key used for encryption
+        byte[] iv = Encoding.UTF8.GetBytes("1234567890ABCDEF"); // Same IV used for encryption
+
+        // Decrypt the input text
+        string decryptedText = Decrypt(encryptedData, key, iv);
+        Console.WriteLine("Decrypted: " + decryptedText);
+    }
+}
---
+++ Encrypt/Encrypt/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class EncryptionProgram
+{
+    // Encrypt data using AES algorithm
+    public static byte[] Encrypt(string plainText, byte[] key, byte[] iv)
+    {
+        // Create AES encryption algorithm instance
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            // Create encryptor using AES algorithm
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create memory stream to store encrypted data
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                // Create crypto stream to perform encryption
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    // Write encrypted data to crypto stream
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                }
+                // Return encrypted data as byte array
+                return msEncrypt.ToArray();
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the text you want to encrypt:");
+        string originalText = Console.ReadLine();
+
+        // Define encryption key and initialization vector (IV)
+        byte[] key = Encoding.UTF8.GetBytes("0123456789ABCDEF"); // 16-byte key for AES-128
+        byte[] iv = Encoding.UTF8.GetBytes("1234567890ABCDEF"); // 16-byte IV (Initialization Vector)
+
+        // Encrypt the input text
+        byte[] encryptedBytes = Encrypt(originalText, key, iv);
+        string encryptedText = Convert.ToBase64String(encryptedBytes);
+        Console.WriteLine("Encrypted: " + encryptedText);
+
+        // Pass encrypted text to decryption program
+        Console.WriteLine("\nPass this encrypted text to the decryption program:");
+        Console.WriteLine(encryptedText);
+    }
+}
